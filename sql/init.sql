e3788a40b756d4d28b43825390d5e47963827684
Claudia Darás <claudiadaras@gmail.com>
2022-11-18 15:12:09 +0100

    Tablas

--- sql/init.sql
+++ sql/init.sql
@@ -1,9 +1,42 @@
 --
 --PostgreSQL database 
 --
-CREATE TABLE student
+
+CREATE TABLE cliente
 (
     id bigint NOT NULL,
-    name text COLLATE pg_catalog."default",
-    CONSTRAINT student_pkey PRIMARY KEY (id)
+    nombre varchar(15) ,
+    apellido varchar(30),
+    CONSTRAINT cliente_pkey PRIMARY KEY (id)
 );
+
+CREATE TABLE producto
+(
+    id bigint NOT NULL,
+    nombre varchar(15)
+    stock INT NOT NULL,
+    color varchar(10) NOT NULL,
+    material varchar(10),
+    precio decimal NOT NULL,
+    CONSTRAINT producto_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE venta
+(
+    id bigint NOT NULL,
+    id_cliente bigint NOT NULL,
+    id_producto bigint NOT NULL,
+    unidadesProducto INT NOT NULL,
+    fecha timestamp NOT NULL,
+    CONSTRAINT venta_pkey PRIMARY KEY (id)
+);
+
+alter table venta
+   add constraint FK_venta_cliente
+   foreign key (id_cliente)
+   references cliente(id);
+   
+alter table venta
+   add constraint FK_venta_producot
+   foreign key (id_producto)
+   references producto(id);
