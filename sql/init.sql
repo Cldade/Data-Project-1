867d778159ff470debc37c2ba0b8d2e1b5044405
Claudia Darás <claudiadaras@gmail.com>
2022-11-18 16:59:11 +0100

    El bueno

--- sql/init.sql
+++ sql/init.sql
@@ -10,16 +10,34 @@ CREATE TABLE cliente
     CONSTRAINT cliente_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE producto
+CREATE TABLE influencer
 (
     id bigint NOT NULL,
-    nombre varchar(15)
+    nombre varchar(15) ,
+    apellido varchar(30),
+    porcentaje decimal NOT NULL,
+    CONSTRAINT influencer_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE producto
+(
+    id_producto bigint NOT NULL,
+    nombre varchar(15),
     stock INT NOT NULL,
     color varchar(10) NOT NULL,
-    material varchar(10),
-    precio decimal NOT NULL,
+    precioVenta decimal NOT NULL,
+    coste decimal NOT NULL,
+    categoria varchar(20),
+    id_influencer int,
+    link_producto varchar(300),
+    link_linea varchar(300),
     CONSTRAINT producto_pkey PRIMARY KEY (id)
 );
+
+alter table producto
+   add constraint FK_venta_cliente
+   foreign key (id_influencer)
+   references influencer(id);
 
 CREATE TABLE venta
 (
@@ -28,6 +46,7 @@ CREATE TABLE venta
     id_producto bigint NOT NULL,
     unidadesProducto INT NOT NULL,
     fecha timestamp NOT NULL,
+    beneficioInfluencer decimal,
     CONSTRAINT venta_pkey PRIMARY KEY (id)
 );
 
@@ -40,3 +59,12 @@ alter table venta
    add constraint FK_venta_producot
    foreign key (id_producto)
    references producto(id);
+
+CREATE TABLE factura
+(
+    id bigint NOT NULL,
+    fecha date NOT NULL,
+    unidades bigint NOT NULL,
+    importanteTotal decimal NOT NULL,
+    CONSTRAINT factura_pkey PRIMARY KEY (id)
+);
